cbea46f3f7e6fea36c1db9af3a5776fbc8a42187
hanbinghjl <hanjianlong@rongshangsoft.com>
2014-06-24 09:46:41 +0800

    落掉

---
+++ src1.2/main/java/platform/view/build/db/DataResultSet.java
@@ -0,0 +1,16 @@
+//Source file: C:\\Program Files\\SQLLIB\\bin\\com\\zt\\db\\DataResultSet.java
+
+package platform.view.build.db;
+
+
+public class DataResultSet 
+{
+   
+   /**
+   @roseuid 3D6D024C014D
+    */
+   public DataResultSet() 
+   {
+    
+   }
+}
