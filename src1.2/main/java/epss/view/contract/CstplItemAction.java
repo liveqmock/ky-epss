644eee54551248706c029e761d7c56708c3fc8d9
hanbinghjl <hanjianlong@rongshangsoft.com>
2014-07-22 19:36:07 +0800

    new

--- src1.2/main/java/epss/view/contract/CstplItemAction.java
+++ src1.2/main/java/epss/view/contract/CstplItemAction.java
@@ -669,9 +669,8 @@ public class CstplItemAction {
             cstplItemShowListExcel =new ArrayList<CstplItemShow>();
             for(CstplItemShow itemUnit: cstplItemShowList){
                 CstplItemShow itemUnitTemp= (CstplItemShow) BeanUtils.cloneBean(itemUnit);
-                String correspondingStrNoContrast=ToolUtil.getStrIgnoreNull(itemUnit.getStrNoContrast()).replaceAll("&#8195;","");
                 itemUnitTemp.setStrNo(ToolUtil.getIgnoreSpaceOfStr(itemUnitTemp.getStrNo()));
-                itemUnitTemp.setStrNoContrast(correspondingStrNoContrast);
+                itemUnitTemp.setStrNoContrast(ToolUtil.getIgnoreSpaceOfStr(itemUnitTemp.getStrNoContrast()));
                 cstplItemShowListExcel.add(itemUnitTemp);
             }
             beansMap.put("cstplItemShowListExcel", cstplItemShowListExcel);
