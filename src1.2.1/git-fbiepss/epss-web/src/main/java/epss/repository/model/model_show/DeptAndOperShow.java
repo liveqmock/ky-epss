8bda309f4ccfc9517796f78b031b30e5b57e41b0
rsyuxiangyang <yuxiangyang@rongshangsoft.com>
2014-08-12 13:44:37 +0800

    new

---
+++ src1.2.1/git-fbiepss/epss-web/src/main/java/epss/repository/model/model_show/DeptAndOperShow.java
@@ -0,0 +1,43 @@
+package epss.repository.model.model_show;
+
+/**
+ * Created by XIANGYANG on 2014/8/11.
+ */
+public class DeptAndOperShow {
+    private String pkid;
+    private String id;
+    private String name;
+    private String type;
+
+    public String getPkid() {
+        return pkid;
+    }
+
+    public void setPkid(String pkid) {
+        this.pkid = pkid;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
