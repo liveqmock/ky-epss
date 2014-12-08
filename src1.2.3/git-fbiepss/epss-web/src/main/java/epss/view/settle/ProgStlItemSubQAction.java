6f8836fa36d15500d33129f0a5f60eef878efb9a
kyinfopuyu <qqace12@126.com>
2014-12-08 15:13:47 +0800

    分包进度工程量结算增加“附件”

--- src1.2.3/git-fbiepss/epss-web/src/main/java/epss/view/settle/ProgStlItemSubQAction.java
+++ src1.2.3/git-fbiepss/epss-web/src/main/java/epss/view/settle/ProgStlItemSubQAction.java
@@ -1,30 +1,33 @@
 package epss.view.settle;
 
 import epss.common.enums.*;
+import epss.repository.model.model_show.AttachmentModel;
 import epss.repository.model.model_show.ProgStlInfoShow;
 import epss.repository.model.model_show.ProgStlItemSubQShow;
-import skyline.util.JxlsManager;
+import org.apache.commons.lang.StringUtils;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+import org.primefaces.model.UploadedFile;
 import skyline.util.ToolUtil;
 import epss.repository.model.*;
 import epss.service.*;
 import epss.view.flow.EsCommon;
 import epss.view.flow.EsFlowControl;
-import jxl.write.WriteException;
 import org.apache.commons.beanutils.BeanUtils;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import skyline.util.MessageUtil;
+import javax.activation.MimetypesFileTypeMap;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlGraphicImage;
 import javax.faces.context.FacesContext;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
 import java.util.*;
-
 /**
  * Created with IntelliJ IDEA.
  * User: Think
@@ -70,7 +73,9 @@ public class ProgStlItemSubQAction {
     private List<ProgStlItemSubQShow> progStlItemSubQShowListExcel;
     private Map beansMap;
     private String strFlowStatusRemark;
-
+    private HtmlGraphicImage image;
+    private StreamedContent downloadFile;
+    private List<AttachmentModel> attachmentList;
     @PostConstruct
     public void init() {
         try {
@@ -133,6 +138,8 @@ public class ProgStlItemSubQAction {
             }
             progStlItemSubQShowList =new ArrayList<>();
             progStlItemSubQShowListExcel =new ArrayList<>();
+            attachmentList=new ArrayList<>();
+            attachmentList=ToolUtil.getListAttachmentByStrAttachment(progStlInfoShow.getAttachment());
             recursiveDataTable("root", cttItemList, progStlItemSubQShowList);
             progStlItemSubQShowList =getStlSubCttEngQMngConstructList_DoFromatNo(progStlItemSubQShowList);
             setItemOfEsItemHieRelapList_AddTotal();
@@ -621,17 +628,104 @@ public class ProgStlItemSubQAction {
             MessageUtil.addError(e.getMessage());
         }
     }
-    public String onExportExcel()throws IOException, WriteException {
-        if (this.progStlItemSubQShowList.size() == 0) {
-            MessageUtil.addWarn("记录为空...");
-            return null;
-        } else {
-            String excelFilename = "分包数量结算-" + new SimpleDateFormat("yyyyMMdd").format(new Date()) + ".xls";
-            JxlsManager jxls = new JxlsManager();
-            jxls.exportList(excelFilename, beansMap,"progStlItemSubQ.xls");
-            // 其他状态的票据需要添加时再修改导出文件名
+    // 附件
+    public void onViewAttachment(AttachmentModel attachmentModelPara) {
+        image.setValue("/upload/stl/SubQ/" + attachmentModelPara.getCOLUMN_NAME());
+    }
+    public void delAttachmentRecordAction(AttachmentModel attachmentModelPara){
+        try {
+            File deletingFile = new File(attachmentModelPara.getCOLUMN_PATH());
+            deletingFile.delete();
+            attachmentList.remove(attachmentModelPara) ;
+            StringBuffer sbTemp = new StringBuffer();
+            for (AttachmentModel item : attachmentList) {
+                sbTemp.append(item.getCOLUMN_PATH() + ";");
+            }
+            progStlInfoShow.setAttachment(sbTemp.toString());
+            progStlInfoService.updateRecord(progStlInfoShow);
+        } catch (Exception e) {
+            logger.error("删除数据失败，", e);
+            MessageUtil.addError(e.getMessage());
         }
-        return null;
+    }
+    public void download(String strAttachment){
+        try{
+            if(StringUtils.isEmpty(strAttachment) ){
+                MessageUtil.addError("路径为空，无法下载！");
+                logger.error("路径为空，无法下载！");
+            }
+            else {
+                String fileName=FacesContext.getCurrentInstance().getExternalContext().getRealPath("/upload/stl/SubQ")+"/"+strAttachment;
+                File file = new File(fileName);
+                InputStream stream = new FileInputStream(fileName);
+                downloadFile = new DefaultStreamedContent(stream, new MimetypesFileTypeMap().getContentType(file), new String(strAttachment.getBytes("gbk"),"iso8859-1"));
+            }
+        } catch (Exception e) {
+            logger.error("下载文件失败", e);
+            MessageUtil.addError("下载文件失败,"+e.getMessage()+strAttachment);
+        }
+    }
+    public void upload(FileUploadEvent event) {
+        BufferedInputStream inStream = null;
+        FileOutputStream fileOutputStream = null;
+        UploadedFile uploadedFile = event.getFile();
+        AttachmentModel attachmentModel = new AttachmentModel();
+        if (uploadedFile != null) {
+            String path = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/upload/stl/SubQ");
+            File superFile = new File(path);
+            if (!superFile.exists()) {
+                superFile.mkdirs();
+            }
+            File descFile = new File(superFile, uploadedFile.getFileName());
+            attachmentModel.setCOLUMN_ID(ToolUtil.getIntIgnoreNull(attachmentList.size()) + "");
+            attachmentModel.setCOLUMN_NAME(uploadedFile.getFileName());
+            attachmentModel.setCOLUMN_PATH(descFile.getAbsolutePath());
+            for (AttachmentModel item : attachmentList){
+                if (item.getCOLUMN_NAME().equals(attachmentModel.getCOLUMN_NAME())) {
+                    MessageUtil.addError("附件已存在！");
+                    return;
+                }
+            }
+
+            attachmentList.add(attachmentModel);
+
+            StringBuffer sb = new StringBuffer();
+            for (AttachmentModel item : attachmentList) {
+                sb.append(item.getCOLUMN_NAME() + ";");
+            }
+            if(sb.length()>4000){
+                MessageUtil.addError("附件路径("+sb.toString()+")长度已超过最大允许值4000，不能入库，请联系系统管理员！");
+                return;
+            }
+            progStlInfoShow.setAttachment(sb.toString());
+            progStlInfoService.updateRecord(progStlInfoShow);
+            try {
+                inStream = new BufferedInputStream(uploadedFile.getInputstream());
+                fileOutputStream = new FileOutputStream(descFile);
+                byte[] buf = new byte[1024];
+                int num;
+                while ((num = inStream.read(buf)) != -1) {
+                    fileOutputStream.write(buf, 0, num);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (inStream != null) {
+                    try {
+                        inStream.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+                if (fileOutputStream != null) {
+                    try {
+                        fileOutputStream.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        }
     }
     /* 智能字段Start*/
     public CttInfoService getCttInfoService() {
@@ -769,6 +863,31 @@ public class ProgStlItemSubQAction {
     public void setStrFlowStatusRemark(String strFlowStatusRemark) {
         this.strFlowStatusRemark = strFlowStatusRemark;
     }
+
+    public HtmlGraphicImage getImage() {
+        return image;
+    }
+
+    public void setImage(HtmlGraphicImage image) {
+        this.image = image;
+    }
+
+    public StreamedContent getDownloadFile() {
+        return downloadFile;
+    }
+
+    public void setDownloadFile(StreamedContent downloadFile) {
+        this.downloadFile = downloadFile;
+    }
+
+    public List<AttachmentModel> getAttachmentList() {
+        return attachmentList;
+    }
+
+    public void setAttachmentList(List<AttachmentModel> attachmentList) {
+        this.attachmentList = attachmentList;
+    }
+
     /*智能字段End*/
 }
 
