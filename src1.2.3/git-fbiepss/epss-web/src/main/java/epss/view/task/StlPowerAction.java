943f1d102f7851c5270865d91d1e948c0110d77c
kyinfopuyu <qqace12@126.com>
2014-12-17 09:32:04 +0800

    结算权限标签页面实现treeTable显示

--- src1.2.3/git-fbiepss/epss-web/src/main/java/epss/view/task/StlPowerAction.java
+++ src1.2.3/git-fbiepss/epss-web/src/main/java/epss/view/task/StlPowerAction.java
@@ -1,5 +1,6 @@
 package epss.view.task;
 
+import epss.common.enums.EnumResType;
 import epss.repository.model.model_show.TaskShow;
 import epss.service.TaskService;
 import org.primefaces.model.DefaultTreeNode;
@@ -36,14 +37,56 @@ public class StlPowerAction {
         //整个任务列表
         try {
             stlPowerRoot = new DefaultTreeNode("ROOT", null);
-            stlPowerList=taskService.initRecentlyPowerTaskShowList();
-            for (int i=0;i<stlPowerList.size();i++){
-
-            }
-
+            TaskShow taskShow=new TaskShow();
+            taskShow.setId("结算权限");
+            TreeNode stlPower= new DefaultTreeNode(taskShow, stlPowerRoot);
+            stlPower.setExpanded(true);
+            TaskShow taskShowFront1 = new TaskShow();
+            taskShowFront1.setId("分包进度结算");
+            TreeNode subStl = new DefaultTreeNode(taskShowFront1, stlPower);
+            TaskShow taskShowFront2 = new TaskShow();
+            taskShowFront2.setId("工程量结算");
+            TreeNode subStlQ = new DefaultTreeNode(taskShowFront2, subStl);
+            getChildNode(EnumResType.RES_TYPE3.getCode(), subStlQ);
+            TaskShow taskShowFront3 = new TaskShow();
+            taskShowFront3.setId("材料消耗量结算");
+            TreeNode subStlM = new DefaultTreeNode(taskShowFront3, subStl);
+            getChildNode(EnumResType.RES_TYPE4.getCode(), subStlM);
+            TaskShow taskShowFront4 = new TaskShow();
+            taskShowFront4.setId("费用结算");
+            TreeNode subStlF = new DefaultTreeNode(taskShowFront4, subStl);
+            getChildNode(EnumResType.RES_TYPE8.getCode(), subStlF);
+            TaskShow taskShowFront5 = new TaskShow();
+            taskShowFront5.setId("总包进度结算");
+            TreeNode tkStl = new DefaultTreeNode(taskShowFront5, stlPower);
+            TaskShow taskShowFront6 = new TaskShow();
+            taskShowFront6.setId("工程量统计结算");
+            TreeNode tkStlEst = new DefaultTreeNode(taskShowFront6, tkStl);
+            getChildNode(EnumResType.RES_TYPE6.getCode(), tkStlEst);
+            TaskShow taskShowFront7 = new TaskShow();
+            taskShowFront7.setId("工程量计量结算");
+            TreeNode tkStlMea = new DefaultTreeNode(taskShowFront7, tkStl);
+            getChildNode(EnumResType.RES_TYPE7.getCode(), tkStlMea);
         }catch (Exception e){
             logger.error("初始化失败", e);
         }
+    }
+
+    private TreeNode getChildNode(String typePara,TreeNode parentNode)throws Exception {
+        stlPowerList=taskService.initRecentlyPowerTaskShowList();
+        for (int i=0;i<stlPowerList.size();i++){
+           TaskShow taskShow=new TaskShow();
+           TaskShow taskShowTemp=stlPowerList.get(i);
+           if(typePara.equals(taskShowTemp.getType())){
+               taskShow.setPkid(taskShowTemp.getPkid());
+               taskShow.setId(taskShowTemp.getId());
+               taskShow.setType(taskShowTemp.getType());
+               taskShow.setName(taskShowTemp.getName());
+               taskShow.setSignPartBName(taskShowTemp.getSignPartBName());
+               return  new DefaultTreeNode(taskShow, parentNode);
+           }
+        }
+        return null;
     }
 
     public TreeNode getStlPowerRoot() {
